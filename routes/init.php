86c6c9938beb8a87f5f40817ca8364a64c07c2a6
Ryan C. Durham <ryan@stagerightlabs.com>
2021-01-16 11:47:24 -0800

    Set up initializationo routes outside of web middleware stack.

---
+++ routes/init.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Initialization Routes
+|--------------------------------------------------------------------------
+*/
+
+if (app()->environment('local')) {
+    Route::get('info', function () {
+        phpinfo();
+    });
+}
+
+Route::get('/', function() {
+    return 'Hello world';
+});
