f7faeabd5027bc025c3859951cedfadf7c0af61d
Ryan C. Durham <ryan@stagerightlabs.com>
2021-01-16 11:08:37 -0800

    Frontend asset pipeline: parcel, postcss and tailwind

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+// postcss.config.js
+module.exports = {
+  plugins:
+    process.env.NODE_ENV === 'production'
+      ? [
+          require('postcss-import'),
+          require('tailwindcss'),
+          require('autoprefixer'),
+          require('cssnano')({
+            preset: 'default'
+          })
+        ]
+      : [
+          require('postcss-import'),
+          require('tailwindcss'),
+          require('autoprefixer')
+        ]
+}
